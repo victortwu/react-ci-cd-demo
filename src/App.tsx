0f182bd69f439023b3c884c1f25026421a3362fe
Victor Twu <victortwu@Victors-MacBook-Pro.local>
2022-09-16 15:11:29 -0700

    changed title, added MUI button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There!</h1>
-
+      <h1>react-firebase-ci-cd-demo</h1>
+      <Button onClick={()=> alert('Hello There!')} variant='contained' size='large'>I'm a MUI Button</Button>
     </div>
   );
 }
