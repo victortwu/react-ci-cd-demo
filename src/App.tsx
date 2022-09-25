a0be295c6f299b235d56ded5863e0e466d9c6586
Victor Twu <victortwu@Victors-MBP.lan>
2022-09-24 22:23:11 -0700

    created email feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import { revenues } from './data/revenues.data'
 import { getTotal } from './utils/getTotal.utils';
+import { getHtml } from './utils/getHtml.utils';  
+import emailjs from '@emailjs/browser'
 import './App.css';
 
 function App() {
   const [total, setTotal] = useState('')
+
+  const stringListArray = revenues.map(rev=> {
+    return `<li style="width:33vw;list-style:none;margin:.5rem;padding:.5rem;background-color:#5579c6;color:white;border-radius:.3rem;">${rev}</li>`
+  })
+
+  const stringifiedElements = stringListArray.join('')
+  
+  const sendEmail = () => {
+    emailjs.send(
+      'service_qa1tglc',
+      'template_kdsubla',
+      { my_html: getHtml(stringifiedElements) },
+      'user_DpQPC7Fuoft6fQqeSq44F')
+      .then(res => console.log(res))
+      .catch(e => console.error(e))
+  }
 
   return (
     <div className="App">
@@ -19,11 +37,12 @@ function App() {
         color: 'green'
       }}>
         {
-          revenues.map((amt,i)=> (<span key={`${i}-${amt}`} style={{margin: '0.2rem'}}>{amt}</span>))
+          revenues.map((amt, i) => (<span key={`${i}-${amt}`} style={{ margin: '0.2rem' }}>{amt}</span>))
         }
       </div>
       <Button data-testid='abc123' onClick={() => setTotal(getTotal(revenues))} variant='contained' size='large'>Get Total</Button>
-        <p>Total: <span style={{color: 'green'}}>{total}</span></p>
+      <p>Total: <span style={{ color: 'green' }}>{total}</span></p>
+      <Button onClick={sendEmail} variant='contained' size='large'>Send Email</Button>
     </div>
   );
 }
